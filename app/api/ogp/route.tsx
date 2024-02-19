e5831a79b92252ffbad33920fb5906e4f51b40be
mktu <mushi9ui@gmail.com>
2024-02-20 01:33:30 +0900

    fix logo

--- app/api/ogp/route.tsx
+++ app/api/ogp/route.tsx
@@ -1,5 +1,6 @@
 import { ImageResponse } from 'next/og'
 import SpeedIcon from '@/components/icons/speed'
+import Logo from '@/components/icons/logo'
 
 export const runtime = 'edge'
 
@@ -7,7 +8,6 @@ export async function GET() {
     return new ImageResponse(
         (
             <div style={{
-                fontSize: 92,
                 background: 'white',
                 width: '100%',
                 height: '100%',
@@ -15,21 +15,11 @@ export async function GET() {
                 textAlign: 'center',
                 alignItems: 'center',
                 justifyContent: 'center',
-                gap: '1rem'
             }}>
-                <SpeedIcon
-                    style={{
-                        width: '200px',
-                        height: '200px',
-
-                    }} />
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}>
-                    <div>ポケモン</div>
-                    <div>速度比較</div>
-                </div>
+                <Logo style={{
+                    width: '600px',
+                    height: '200px'
+                }} />
             </div>
         ),
         {
